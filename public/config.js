954c750a34e7c77e2ff8b6bbb45415b4a8c7e0b6
Luke Schlangen <lukeschlangen@gmail.com>
2016-09-30 11:49:18 -0500

    Adding config.js for reuse clarity

---
+++ public/config.js
@@ -0,0 +1,11 @@
+// You will need to update these with your own values
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBql0odmgyLvsCsi2xC-VbDj5eZieoYT0A",
+  authDomain: "firenode-155ef.firebaseapp.com",
+  databaseURL: "https://firenode-155ef.firebaseio.com",
+  storageBucket: "firenode-155ef.appspot.com",
+  messagingSenderId: "669774409440"
+};
+firebase.initializeApp(config);
